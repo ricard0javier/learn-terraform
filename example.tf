269c6d2c85fb4af6839ef8ae013485595c9ae0ff
rvilla145 <ricardo_villanueva@comcast.com>
2018-07-05 12:22:24 +0100

    provisioning infrastructure

--- example.tf
+++ example.tf
@@ -8,26 +8,12 @@ resource "aws_instance" "example" {
   ami           = "ami-b374d5a5"
   instance_type = "t2.micro"
 
-  # Tells Terraform that this EC2 instance must be created only after the
-  # S3 bucket has been created.
-  depends_on = ["aws_s3_bucket.example"]
-
+  provisioner "local-exec" {
+    command = "echo ${aws_instance.example.public_ip} > ip_address.txt"
+  }
 }
 
 resource "aws_eip" "ip" {
   instance = "${aws_instance.example.id}"
 }
 
-# New resource for the S3 bucket our application will use.
-resource "aws_s3_bucket" "example" {
-  # NOTE: S3 bucket names must be unique across _all_ AWS accounts, so
-  # this name must be changed before applying this example to avoid naming
-  # conflicts.
-  bucket = "cts-intl-ps-rvillanueva-terraform-getting-started-guide"
-  acl    = "private"
-}
-
-resource "aws_instance" "another" {
-  ami           = "ami-b374d5a5"
-  instance_type = "t2.micro"
-}
